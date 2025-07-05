3d72515fc6b464b778fff325222a6a9ca1e4da7b
dentinhosdeleiteodontologiaoficial <dr.rafaelmoreiraped@gmail.com>
2025-07-05 01:17:17 -0300

    Update create_db.py

--- backend/create_db.py
+++ backend/create_db.py
@@ -1,7 +1,10 @@
-import os
-from app import app, db # Importa a instância do app e db do seu app.py
+from app import db, app # Importa db e app do seu app.py
 
 with app.app_context():
+    print("Tentando apagar todas as tabelas do banco de dados (se existirem)...")
+    db.drop_all() # Apaga todas as tabelas existentes
+    print("Tabelas apagadas.")
+
     print("Tentando criar todas as tabelas do banco de dados...")
-    db.create_all()
+    db.create_all() # Cria todas as tabelas definidas nos modelos
     print("Tabelas criadas ou já existentes.")
